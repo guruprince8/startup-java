0a1a1ed5872f2c478b8a4e5aa70ea61616e73c8c
guruprince8 <guru.prince8@gmail.com>
2021-09-12 18:56:20 +0530

    adding javadoc

--- Startup.java
+++ Startup.java
@@ -1,4 +1,11 @@
+/**
+ * written by guru
+ */
 public class Startup {
+    /**
+     * starting method
+     * @param args
+     */
     public static void main(String args[]){
         System.out.println("Hello World");
     }
---
+++ member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"Startup","l":"main(String[])","url":"main(java.lang.String[])"},{"p":"<Unnamed>","c":"Startup","l":"Startup()","url":"%3Cinit%3E()"}]
